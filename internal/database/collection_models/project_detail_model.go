504ae2b57ba0e24035c3507332cea5bfb3379f44
ChuongPT <chuongpt@ikameglobal.com>
2025-09-30 22:31:35 +0700

    add project detail CRUD

--- internal/database/collection_models/project_detail_model.go
+++ internal/database/collection_models/project_detail_model.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -26,4 +27,42 @@ func InstertNewProjectDetailToDatabase(client *mongo.Client, url, dbName, collNa
 	collection := client.Database(dbName).Collection(collName)
 	_, err := collection.InsertOne(ctx, projectDetail)
 	return err
+}
+
+func UpdateProjectDetailToDatabase(client *mongo.Client, url, dbName, collName string, projectDetail *ProjectDetail) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	collection := client.Database(dbName).Collection(collName)
+	_, err := collection.UpdateOne(ctx, bson.M{"project": projectDetail.Project}, bson.M{"$set": projectDetail})
+	return err
+}
+
+func DeleteProjectDetailInDatabase(client *mongo.Client, url, dbName, collName string, project string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	collection := client.Database(dbName).Collection(collName)
+
+	_, err := collection.DeleteOne(ctx, bson.M{"project": project})
+	return err
+}
+
+func GetAllProjectDetails(client *mongo.Client, url, dbName, collName string) ([]ProjectDetail, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	collection := client.Database(dbName).Collection(collName)
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var results []ProjectDetail
+	for cursor.Next(ctx) {
+		var projectDetail ProjectDetail
+		if err := cursor.Decode(&projectDetail); err != nil {
+			return nil, err
+		}
+		results = append(results, projectDetail)
+	}
+	return results, nil
 }
